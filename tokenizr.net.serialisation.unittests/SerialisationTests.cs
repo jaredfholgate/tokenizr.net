6e07324d38de36c8537f545180444de97f7e4065
jaredfholgate <jaredfholgate@gmail.com>
2018-10-09 14:22:03 -0400

    Add basic json serialisation

---
+++ tokenizr.net.serialisation.unittests/SerialisationTests.cs
@@ -0,0 +1,39 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using tokenizr.net.constants;
+using tokenizr.net.generator;
+
+namespace tokenizr.net.serialisation.unittests
+{
+  [TestClass]
+  public class SerialisationTests
+  {
+    [TestMethod]
+    public void CanSerliaiseTable()
+    {
+      var serialiser = new Serialisation();
+      var generator = new TableGenerator(new GeneratorSettings { Alphabet = Alphabet.English, Size = 1000 });
+      var table = generator.Generate();
+      var result = serialiser.Serliaise(table);
+      var expectedText = "{\"ForwardTable\":[{\"a\":{\"Item1\":\"" + table.ForwardTable[0]['a'].Item1 + "\",\"Item2\":" + table.ForwardTable[0]['a'].Item2 + "},\"b\":{\"Item1\":\"" + table.ForwardTable[0]['b'].Item1 + "\",\"Item2\":" + table.ForwardTable[0]['b'].Item2 + "},";
+      Assert.IsTrue(result.Contains(expectedText));
+    }
+
+    [TestMethod]
+    public void CanDeSerialiseTable()
+    {
+      var serialiser = new Serialisation();
+      var generator = new TableGenerator(new GeneratorSettings { Alphabet = Alphabet.English, Size = 1000 });
+      var table = generator.Generate();
+      var result = serialiser.Serliaise(table);
+      var resultTable = serialiser.Deserialise(result);
+      for (var i = 0; i < 1000; i++)
+      {
+        foreach (var character in Alphabet.English)
+        {
+          Assert.AreEqual(table.ForwardTable[i][character].Item1, resultTable.ForwardTable[i][character].Item1);
+          Assert.AreEqual(table.ForwardTable[i][character].Item2, resultTable.ForwardTable[i][character].Item2);
+        }
+      }
+    }
+  }
+}
---
+++ tokenizr.net.serialisation/Serialisation.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using tokenizr.net.structures;
+
+namespace tokenizr.net.serialisation
+{
+  public class Serialisation
+  {
+    public string Serliaise(TokenTableSet table)
+    {
+      return JsonConvert.SerializeObject(table);
+    }
+
+    public TokenTableSet Deserialise(string table)
+    {
+      return JsonConvert.DeserializeObject<TokenTableSet>(table);
+    }
+  }
+}
